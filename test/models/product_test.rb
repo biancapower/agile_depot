61a0fd356045af4e092f705a72d34d86535aa704
biancapower <bianca@biancapower.com>
2022-05-15 12:15:22 +1000

    add test of unique title using fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -52,4 +52,14 @@ class ProductTest < ActiveSupport::TestCase
             "#{image_url} must be invalid"
     end
   end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy", 
+                          price:       1, 
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
 end
