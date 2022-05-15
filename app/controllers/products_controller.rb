a5b365dff66cd5aeedc9a3e2cb0b5e82e1a049fd
biancapower <bianca@biancapower.com>
2022-05-15 12:28:33 +1000

    add code to puts full error message to test output

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,9 @@ class ProductsController < ApplicationController
         format.html { redirect_to product_url(@product), notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @product }
       else
+        # output full error message to console
+        # this is displayed in test output also
+        # puts @product.errors.full_messages
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
@@ -41,6 +44,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
       else
+        # puts @product.errors.full_messages
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
